1f00beed828c7e0a6069c7c0650fc9ee8a136978
tanjil <rahman35uap@gmail.com>
2019-10-28 13:51:36 +0900

    New feature(Power-4) has been calculated

--- number.cpp
+++ number.cpp
@@ -3,6 +3,6 @@ int main()
 {
     for(int i;i<10;i++)
     {
-        printf("%d\n",i*i*i);     /// Calculating the cube of the number
+        printf("%d\n",i*i*i*i);     /// Calculating the power 4 of the number
     }
 }
